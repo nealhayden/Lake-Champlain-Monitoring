0a1d796e81754ded4ad0e96bbd9d3cca194e6aef
Neal.Hayden <Neal.Hayden@kdrp.com>
2019-05-18 20:27:17 -0400

    Updated for Arima.

--- README.md
+++ README.md
@@ -5,6 +5,10 @@
 - Department of Environmental Conservation: [Lake Champlain Long Term Monitoring Project](https://dec.vermont.gov/watershed/lakes-ponds/monitor/lake-champlain)
 
 ### Data Exploration
-- The correlations between various water quality parameters and the amount of chlorophyll are plotted below.
+- I plotted the correlations between various water quality parameters and the amount of chlorophyll below.
 The relationship between total phosphorus and chlorophyll is moderately strong (R avg = 0.51). 
 ![Correlations with chlorophyll](Correlations.JPG)
+
+### Predictions
+-- Predictions for chlorophyll amounts at each testing station for the 2019-2021.
+![chlorophyll prediction](arima.JPG)
--- main.R
+++ main.R
@@ -1,8 +1,9 @@
 library(tidyverse)
 library(broom)
 library(lubridate)
+library(forecast)
 library(car)
-setwd('C:/Users/nhayden/OneDrive/work/Lake-Champlain-Monitoring')
+
 rm(list = ls())
 ##==========================================================================================================================================
 ## Intro-
@@ -38,11 +39,16 @@ add_rownames <- function(df) {
 by_station <- by_station %>% mutate(chlorophyll_correlation = map(data,test_correlations)%>% map(add_rownames)) 
 cors <- unnest(by_station, chlorophyll_correlation )
 
-## Plot Correlations with Chlorophyll. 
+## Make a table of average correlation with chlorophyll. 
+cors %>% group_by(test_name) %>% summarize(avg = mean(Chlorophyll_a, na.rm = T)) 
+
+## Plot Correlations with chlorophyll. 
 plot <- ggplot(data = cors, aes(x = Chlorophyll_a)) + geom_histogram(binwidth = 0.1, color="black", fill="grey") + coord_cartesian(xlim = c(-1, 1))
 plot + facet_wrap(~test_name, scale = "free", nrow = 3)
 
-## Build linear models for Chlorophyll amount using Phosphorus. 
+avg(cors$Chlorophyll_a, na.rm=T)
+
+## Build linear models for chlorophyll amount using Phosphorus. 
 linear_model <- function (df) {
   df <- df  %>% spread(Test, medianResult)
   lm(Chlorophyll_a ~ Total_Phosphorus , data = df) %>% tidy()
@@ -63,4 +69,15 @@ for (i in 1:15) {
   plot(by_station$ts_data[[i]])
 }  
 
+## Model time series using ARIMA forecasting. 
+arima_model <- function (ts) {
+  Arima(ts, order = c(0,1,1))
+}
+by_station <- by_station %>% mutate(fit_arima = map(ts_data, arima_model))
+
+## Plot the Arima forecasts.  
+par(mfrow = c(3,5))
+for (i in 1:15) {
+  plot(forecast(by_station$fit_arima[[i]], 3))
+}  
 
